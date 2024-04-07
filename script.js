52d1a57b6fa0318970e5cfacb4472749592f8e4b
Peter Kolosov <101456132+Peter-developer01@users.noreply.github.com>
2024-04-07 14:55:06 +0300

    new cipher

--- script.js
+++ script.js
@@ -1,127 +1,51 @@
-const generateKeysButton = document.querySelector("#generate-keys"),
-	saveKeysButton = document.querySelector("#save-keys"),
-	updateSymbolsButton = document.querySelector("#update-symbols"),
-	resetSymbolsButton = document.querySelector("#reset-to-default"),
+const saveKeyButton = document.querySelector("#save-key"),
 	encryptButton = document.querySelector("#encrypt"),
 	decryptButton = document.querySelector("#decrypt"),
-	cipherArea = document.querySelector("#cipher-area"),
-	symbolsArea = document.querySelector("#symbols"),
+	keyInput = document.querySelector("#key-area"),
 	encryptArea = document.querySelector("#encrypt-area"),
 	decryptArea = document.querySelector("#decrypt-area"),
 	outputArea = document.querySelector("#output");
 
-const defaultSymbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !@#$%^&*()_+-=[]{}|;':\",./<>?`~";
+let key = "";
+loadKey();
 
-let symbols = defaultSymbols;
-
-loadSymbols();
-
-let keys = {};
-
-loadKeys();
-
-generateKeysButton.addEventListener("click", () => {
-	const generatedKeys = generateKeys();
-	keys = generatedKeys;
-	cipherArea.value = JSON.stringify(keys);
-	saveKeys();
-});
-
-saveKeysButton.addEventListener("click", () => {
-	saveKeys();
+saveKeyButton.addEventListener("click", () => {
+	saveKey();
 });
 
 encryptButton.addEventListener("click", () => {
+	checkKey();
 	const encrypted = encrypt(encryptArea.value);
 	outputArea.value = encrypted;
 });
 
 decryptButton.addEventListener("click", () => {
+	checkKey();
 	const decrypted = decrypt(decryptArea.value);
 	outputArea.value = decrypted;
 });
 
-updateSymbolsButton.addEventListener("click", updateSymbols);
-resetSymbolsButton.addEventListener("click", resetSymbols);
-
-function generateKeys() {
-	let usedValues = [];
-	let cipher = {
-		encrypt: {},
-		decrypt: {}
-	};
-
-	const allCharacters = symbols;
-
-	for (let i = 0; i < allCharacters.length; i++) {
-		let random = Math.floor(Math.random() * allCharacters.length);
-		while (usedValues.includes(random)) {
-			random = Math.floor(Math.random() * allCharacters.length);
-		}
-
-		cipher.encrypt[allCharacters[i]] = random;
-		usedValues.push(random);
-
-	}
-
-	const decrypt = {};
-
-	for (let key in cipher.encrypt) {
-		decrypt[cipher.encrypt[key]] = key;
-	}
-
-	cipher.decrypt = decrypt;
-
-	return cipher;
+function saveKey() {
+	key = keyInput.value;
+	localStorage.setItem("key", key);
 }
 
-function saveKeys() {
-	keys = JSON.parse(cipherArea.value);
-	localStorage.setItem("keys", JSON.stringify(keys));
+function loadKey() {
+	let localKey = localStorage.getItem("key");
+	key = localKey || key;
+	keyInput.value = key;
 }
 
-function loadKeys() {
-	let localKeys = localStorage.getItem("keys");
-	keys = JSON.parse(localKeys);
-	cipherArea.value = localKeys;
+function checkKey() {
+	if (key === "") {
+		alert("Please enter a key");
+	}
 }
 
 function encrypt(text) {
-	const cipher = keys.encrypt;
-	let output = [];
-	for (let i = 0; i < text.length; i++) {
-		output.push(cipher[text[i]]?.toString() || text[i]);
-	}
-	
-	return output.join("-");
+	return CryptoJS.AES.encrypt(text, key).toString();
 }
 
 function decrypt(text) {
-	text = text.trim().replaceAll(" ", "-" + keys.encrypt[" "] + "-").split("-");
-	const cipher = keys.decrypt;
-	let output = "";
-	for (let i = 0; i < text.length; i++) {
-		output += cipher[text[i]] || text[i];
-	}
-	
-	return output;
-}
-
-function loadSymbols() {
-	const localSymbols = localStorage.getItem("symbols");
-	symbolsArea.value = localSymbols || defaultSymbols;
-	symbols = localSymbols || defaultSymbols;
-}
-
-function resetSymbols() {
-	symbols = defaultSymbols;
-	localStorage.setItem("symbols", symbols);
-	symbolsArea.value = symbols;
-	generateKeysButton.click();
-}
-
-function updateSymbols() {
-	symbols = symbolsArea.value;
-	localStorage.setItem("symbols", symbols);
-	generateKeysButton.click();
+	return CryptoJS.AES.decrypt(text, key).toString(CryptoJS.enc.Utf8);
 }
